6880a898de9df5c435673485df5f6ff308bfad2f
nvr1998 <nikhilvijayraj.developer@gmail.com>
2025-07-22 15:10:39 +0530

    changed celery

--- snap-study-be/celeryconfig.py
+++ snap-study-be/celeryconfig.py
@@ -1,8 +1,8 @@
 # app/celeryconfig.py
 
-# broker_url = "redis://default:fLkQ9i7JRUFMFK1xuaTcCKRJgNw13dUu@redis-18251.c264.ap-south-1-1.ec2.redns.redis-cloud.com:18251"
-# result_backend = "redis://default:fLkQ9i7JRUFMFK1xuaTcCKRJgNw13dUu@redis-18251.c264.ap-south-1-1.ec2.redns.redis-cloud.com:18251"
+broker_url = "redis://default:fLkQ9i7JRUFMFK1xuaTcCKRJgNw13dUu@redis-18251.c264.ap-south-1-1.ec2.redns.redis-cloud.com:18251"
+result_backend = "redis://default:fLkQ9i7JRUFMFK1xuaTcCKRJgNw13dUu@redis-18251.c264.ap-south-1-1.ec2.redns.redis-cloud.com:18251"
 
 
-broker_url = "redis://localhost:6379/0"
-result_backend = "redis://localhost:6379/0"
+# broker_url = "redis://localhost:6379/0"
+# result_backend = "redis://localhost:6379/0"
